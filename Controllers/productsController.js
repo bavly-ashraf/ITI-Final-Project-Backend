9612ad8128138b0bac6e5e8838db84a4f9306bd3
bavly-ashraf <bavly.ashraf2023@gmail.com>
2023-06-25 18:48:49 +0300

    edits

--- Controllers/productsController.js
+++ Controllers/productsController.js
@@ -2,5 +2,7 @@ require("dotenv").config();
 const AppError = require("../Helpers/AppError");
 const Products = require("../Models/Products");
 
+const getProductsBySearch = (req,res,next)=>{
+}
 
 module.exports = {};
