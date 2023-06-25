156816fa7dbaa800b7defb26913fd6397ed39b2c
kareem132sameir <mangaka13215@gmail.com>
2023-06-25 16:14:42 +0300

    created products schema

--- Controllers/productsController.js
+++ Controllers/productsController.js
@@ -3,4 +3,19 @@ const AppError = require("../Helpers/AppError");
 const Products = require("../Models/Products");
 
 
-module.exports = {};
+////////////////////////////////////get methods//////////////////////////////////
+
+//http://localhost:8080/products
+
+const getProducts = async (req, res, next) => {
+    try {
+      const products = await Products.find().select("-password");
+      if (products.length == 0) return next(new AppError("Products not found"));
+      res.send(products);
+    } catch (error) {
+      return next(error);
+    }
+  };
+  
+
+module.exports = {getProducts};
--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const routes = express.Router();
+const {getProducts}=require('../Controllers/productsController');
+
 
 //methods[get,post,patch,delete]
+
+//////////get methods///////////
+
+routes.get('/',getUsers);
+
+routes.get('/:id',getUsersById);
+
 
 module.exports = routes
