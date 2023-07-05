957e691e7ddafc5d8e8abcda2818d3f4e3ce84d2
Mohamed Nasser <mohamed17nasser@gmail.com>
2023-07-05 04:21:52 +0300

    Update tokenAuth.js

--- Helpers/tokenAuth.js
+++ Helpers/tokenAuth.js
@@ -1,61 +1,3 @@
-// const jwt = require("jsonwebtoken");
-// const AppError = require("../Helpers/AppError");
-// const User = require("../Models/Users");
-// require("dotenv").config();
-
-// const verifyToken = async (req, res, next) => {
-
-//   try {
-//     const token = req.headers.authorization;
-//     if (!token) {
-//       return next(new AppError("Please provide a token", 401));
-//     }
-//     const secretKey = process.env.JWT_SECRET;
-//     const decoded = jwt.verify(token, secretKey);
-
-//     const id = decoded.id;
-//     req.authorizedUser = await User.findById(id);
-//     if (!req.authorizedUser) {
-//       return next(new AppError("User not found or invalid token", 401));
-//     }
-//     req.id = id;
-//     next();
-//   } catch (error) {
-//     return next(new AppError("Invalid token", 401));
-//   }
-// };
-
-// module.exports = verifyToken;
-// const jwt = require("jsonwebtoken");
-// const AppError = require("../Helpers/AppError");
-// const User = require("../Models/Users");
-// require("dotenv").config();
-
-// const verifyToken = async (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization;
-//     if (!token) {
-//       return next(new AppError("Please provide a token", 401));
-//     }
-//     const secretKey = process.env.JWT_SECRET;
-//     const decoded = jwt.verify(token, secretKey);
-//     console.log("hello from tokenverify");
-
-//     const id = decoded.id;
-//     req.authorizedUser = await User.findById(id);
-//     if (!req.authorizedUser) {
-//       return next(new AppError("User not found or invalid token", 401));
-//     }
-//     req.userId = id; // Renamed from req.id to req.userId for clarity
-//     console.log(req.userId);
-//     next();
-//   } catch (error) {
-//     return next(new AppError("Invalid token", 401));
-//   }
-// };
-
-// module.exports = verifyToken;
-
 const jwt = require("jsonwebtoken");
 const AppError = require("../Helpers/AppError");
 const User = require("../Models/Users");
@@ -77,7 +19,7 @@ const verifyToken = async (req, res, next) => {
     if (!req.authorizedUser) {
       return next(new AppError("User not found or invalid token", 401));
     }
-    req.userId = id; // Renamed from req.id to req.userId for clarity
+    req.id = id; // Renamed from req.id to req.userId for clarity
     console.log("Decoded token userId:", req.userId);
     console.log("Decoded token user:", decoded.user);
     console.log("Decoded token roles:", decoded.roles);
