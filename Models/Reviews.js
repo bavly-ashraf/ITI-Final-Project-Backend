afae6394f466d26a2eb59bfa48699ce197a93203
Mohamed Nasser <mohamed17nasser@gmail.com>
2023-07-05 03:50:02 +0300

    fbusers

--- Models/Reviews.js
+++ Models/Reviews.js
@@ -1,34 +1,34 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-//Reviews schema 
+//Reviews schema
 
 const ReviewsSchema = new Schema({
-    reviewContent:{
-        type:String,
-        required: true,
-    },
-    rating: {
-        type: Number,
-        required: true,
-        min: 1,
-        max: 5
-     },
-     productId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Products',
-        required: true
-     },
-     userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'fbUsers',
-        required: true
-     },
-     createdAt: {
-        type: Date,
-        default: Date.now
-     }
-})
+  reviewContent: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  productId: {
+    type: Schema.Types.ObjectId,
+    ref: "Products",
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Review=mongoose.model('Review',ReviewsSchema)
-module.exports=Review;
+const Review = mongoose.model("Review", ReviewsSchema);
+module.exports = Review;
--- db.js
+++ db.js
@@ -4,6 +4,21 @@ require("dotenv").config();
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.DATABASE.replace('<password>',process.env.DATABASE_PASSWORD));
+  await mongoose.connect(
+    process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD)
+  );
   console.log("connected to db");
 }
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// main().catch((err) => console.log(err));
+
+// async function main() {
+//   await mongoose.connect(
+//     // process.env.DATABASE.replace("<password>", process.env.DATABASE_PASSWORD)
+//     process.env.MONGO_URI
+//   );
+
+//   console.log("connected to db");
+// }
