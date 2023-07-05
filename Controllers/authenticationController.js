e11d5c9b68c174a6867ccb872c2bdff629ab65e1
DoniaAmin <doniaamin666@example.com>
2023-07-05 21:46:10 +0200

    edit review controller

--- Controllers/authenticationController.js
+++ Controllers/authenticationController.js
@@ -50,7 +50,7 @@ const getUsersById = async (req, res, next) => {
 const signUp = async (req, res, next) => {
   console.log("signUp");
   try {
-    console.log(req.body);
+    // console.log(req.body);
     const { email, username, role, password, confirmPassword } = req.body;
     console.log(
       "Received user data:",
@@ -62,13 +62,13 @@ const signUp = async (req, res, next) => {
     );
 
     if (!email || !username || !password || !confirmPassword) {
-      console.log("Missing required info");
+      // console.log("Missing required info");
       return next(new AppError("Please enter the required info"));
     }
 
     const user = await User.findOne({ email });
     if (user) {
-      console.log("User email already exists");
+      // console.log("User email already exists");
       return next(new AppError("User email already exists"));
     }
 
@@ -94,7 +94,7 @@ const signUp = async (req, res, next) => {
     newUser.password = undefined;
     res.send({ newUser, token });
 
-    console.log("User signed up successfully");
+    // console.log("User signed up successfully");
   } catch (error) {
     console.log("Error occurred during signup:", error);
     next(error);
@@ -133,22 +133,22 @@ const getUserDataFromToken = async (decodedToken) => {
   // For example, you can use the decoded token's ID to fetch the corresponding user from the database
   const userId = decodedToken.id;
   const user = await User.findById(userId); // Assuming you have a User model
-  console.log("getuserdata", userId);
+  // console.log("getuserdata", userId);
   // Return the user data
   return user;
 };
 const UserData = async (req, res, next) => {
   const { token } = req.body;
-  console.log(token, req.body);
+  // console.log(token, req.body);
 
   try {
     // Verify and decode the token to get the user data and roles
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("hello token");
+    // console.log("hello token");
     // Retrieve user data from the decoded token
     const user = await getUserDataFromToken(decodedToken);
     // user.password = undefined;
-    console.log(user);
+    // console.log(user);
     // Return the user data and roles in the response
     res.status(200).json({
       user,
@@ -158,7 +158,7 @@ const UserData = async (req, res, next) => {
     if (!user)
       return next(new AppError("Email or Passwrods isnt correct", 403));
   } catch (error) {
-    console.error("Error decoding token:", error);
+    // console.error("Error decoding token:", error);
     res.status(500).json({ error: "Failed to decode token" });
   }
 };
@@ -195,15 +195,10 @@ const updateUser = async (req, res, next) => {
   try {
     const userId = req.id;
     const updatedData = req.body;
-
     const user = await User.findById(userId);
-    if (!user) {
-      return next(new AppError("User not found"));
-    }
-
+    if (!user) return next(new AppError("User not found"));
     Object.assign(user, updatedData);
     await user.save();
-
     res.send(user);
   } catch (error) {
     return next(error);
@@ -215,20 +210,12 @@ const updateUser = async (req, res, next) => {
 const deleteUser = async (req, res, next) => {
   try {
     const { userId } = req.body;
-    if (!userId)
-      return next(
-        new AppError("Please provide the ID of the user you want to delete")
-      );
-
+    if (!userId) return next(new AppError("Please provide the ID of the user you want to delete"));
     // Verify if the requester is an admin
     const admin = await User.findById(req.id);
-    if (admin.role !== "admin") {
-      return next(new AppError("Only admins can delete users", 403));
-    }
-
+    if (admin.role !== "admin") return next(new AppError("Only admins can delete users", 403));
     const user = await User.findById(userId);
     if (!user) return next(new AppError("User does not exist"));
-
     await User.deleteOne({ _id: userId });
 
     //     await Post.deleteMany({ userId: user._id });
@@ -241,21 +228,14 @@ const deleteUser = async (req, res, next) => {
 
 ////////////////////////////
 const Logout = async (req, res, next) => {
-  const idd = req.id;
-  console.log(idd);
-
+  const idd = req.id; 
   // Find the user by ID
-
   const user = await User.findById(idd); // Assuming you have a User model
-  console.log("hello from after");
-
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
-
   user.isLogged = false;
   await user.save();
-  console.log(user.isLogged);
   res.status(200).json({ message: "Logout successful" });
 };
 
@@ -265,13 +245,10 @@ const setAddress = async (req, res, next) => {
   try {
     const { address } = req.body;
     const userId = req.id;
-
     // Update the user's address in the database
     const user = await User.findById(userId);
-    if (!user) {
-      return next(new AppError("User not found", 404));
-    }
-
+    if (!user) return next(new AppError("User not found", 404));
+    
     // Update the user's address fields with the new values
     user.address.apartment = address.apartment;
     user.address.floor = address.floor;
